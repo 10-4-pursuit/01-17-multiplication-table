01e2ec01a9e314696f5339989aef8d076be41fa9
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-11 19:21:24 -0500

    Darnishas finished copy

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const number = 5; // Change this number to test with different multiplication ta
 
 // TODO: Use a for loop to create and log the multiplication table for the given number
 
+for (let i = 1; i <=10; i ++) {
+    let result = number * i;
+    console.log(`${number} * ${i} = ${result}`);
+}
